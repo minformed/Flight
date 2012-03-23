1a78b7d06c9ca4495180928d3ea39e6a67e5e26a
Ryan Clancy <ryanclancy000@yahoo.ca>
2012-03-22 22:33:42 -0300

    Regression, going to start clean... ish

--- src/me/ryanclancy000/flight/Flight.java
+++ src/me/ryanclancy000/flight/Flight.java
@@ -1,37 +1,33 @@
 package me.ryanclancy000.flight;
 
-import org.bukkit.ChatColor;
+import java.io.IOException;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Flight extends JavaPlugin {
-    
-    String pre = ChatColor.GOLD + "[Flight] ";
-    ChatColor green = ChatColor.GREEN;
-    
+
     @Override
     public void onDisable() {
     }
-    
+
     @Override
     public void onEnable() {
+        
         getCommand("fly").setExecutor(this);
-    }
-    
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        
-        Player player = (Player) sender;
-        
-        if (args.length == 0) {
-            player.setFlying(true);
-            sender.sendMessage(pre + green + "Flying enabled!");
-            return true;
+
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            this.getLogger().severe("Could not enable Metrics tracking!");
         }
-        
-        return true;
+
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return true;
+    }
 }
