8e9e752cf1ef6d11686899d509410b0ebc2725b9
Ryan Clancy <ryanclancy000@yahoo.ca>
2012-03-23 16:40:09 -0300

    Added more code

--- src/me/ryanclancy000/flight/Flight.java
+++ src/me/ryanclancy000/flight/Flight.java
@@ -1,11 +1,16 @@
 package me.ryanclancy000.flight;
 
 import java.io.IOException;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Flight extends JavaPlugin {
+
+    private FlightCommands cHandler = new FlightCommands(this);
 
     @Override
     public void onDisable() {
@@ -13,8 +18,7 @@ public class Flight extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        
-        getCommand("fly").setExecutor(this);
+        getCommand("flight").setExecutor(this);
 
         try {
             Metrics metrics = new Metrics(this);
@@ -26,8 +30,80 @@ public class Flight extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("flight")) {
+            return doCommand(sender, args);
+        }
 
-        return true;
+        return onCommand(sender, cmd, commandLabel, args);
+    }
+
+    private boolean doCommand(CommandSender sender, String[] args) {
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+        }
+
+        // Help Command
+        
+        if ("help".equalsIgnoreCase(args[0])) {
+
+            isCreative(player);
+
+            if (!sender.hasPermission("flight.help")) {
+                noPerms(sender);
+                return true;
+            }
+
+            this.cHandler.helpCommand(sender, args);
+            return true;
+
+        }
+
+        // Toggle Command
+        
+        if ("toggle".equalsIgnoreCase(args[0])) {
+
+            isCreative(player);
+
+            if (!sender.hasPermission("flight.toggle")) {
+                noPerms(sender);
+                return true;
+            }
+
+            this.cHandler.toggleCommand(sender, args);
+            return true;
+        }
+
+        // Check Command
+        
+        if ("check".equalsIgnoreCase(args[0])) {
+
+            isCreative(player);
+
+            if (!sender.hasPermission("flight.check")) {
+                noPerms(sender);
+                return true;
+            }
+
+            this.cHandler.checkCommand(sender, args);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void noPerms(CommandSender sender) {
+        sender.sendMessage(ChatColor.RED + "You do not have permission for that command...");
+    }
+
+    public boolean isCreative(Player player) {
+
+        if (player.getGameMode() == GameMode.CREATIVE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/me/ryanclancy000/flight/FlightCommands.java
+++ src/me/ryanclancy000/flight/FlightCommands.java
@@ -1,7 +1,9 @@
 package me.ryanclancy000.flight;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class FlightCommands {
 
@@ -9,14 +11,94 @@ public class FlightCommands {
     ChatColor green = ChatColor.GREEN;
     ChatColor red = ChatColor.RED;
     public Flight plugin;
+    
 
     public FlightCommands(Flight instance) {
         plugin = instance;
     }
 
     public void helpCommand(CommandSender sender, String[] args) {
+        
+        if (args.length > 1) {
+            sender.sendMessage(pre + red + "Too many arguments!");
+            return;
+        }
+        
+        sender.sendMessage(pre + green + "Help Menu!");
+        sender.sendMessage(green + "/flight toggle");
     }
 
     public void toggleCommand(CommandSender sender, String[] args) {
+        
+        if (args.length == 1) {
+            
+            Player player = (Player) sender;
+            
+            if (plugin.isCreative(player)) {
+                player.sendMessage(pre + ChatColor.RED + "Cannot change flight mode while in creative!");
+                return;
+            }
+            
+            if (player.getAllowFlight()) {
+                disableFly(player);
+                player.sendMessage(pre + red + "Flying disabled!");
+            } else {
+                enableFly(player);
+                player.sendMessage(pre + green + "Flying enabled!");
+            }
+            
+        }
+        
+    }
+    
+    public void checkCommand(CommandSender sender, String[] args) {
+        
+        Player player = (Player) sender;
+        
+        Player target = Bukkit.getServer().getPlayer(args[1]);
+        
+        if (args.length == 1) {
+         
+            if (flyModeEnabled(player)) {
+                player.sendMessage(pre + green + "Fly mode enabled, but not currently in the air!");
+                return;
+            }
+            
+            if (currentlyFlying(player)) {
+                player.sendMessage(pre + green + "You are currently in flight!");
+            }
+            
+        }
+                
+    }
+    
+    public void enableFly(Player player) {
+        player.setAllowFlight(true);
+        player.setFlying(true);
+    }
+    
+    public void disableFly(Player player) {
+        player.setAllowFlight(false);
+        player.setFlying(false);
+    }
+    
+    public boolean flyModeEnabled(Player player) {
+        
+        if (player.getAllowFlight()) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public boolean currentlyFlying(Player player) {
+        
+        if (player.isFlying()) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
