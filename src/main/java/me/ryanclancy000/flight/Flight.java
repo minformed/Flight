c30d1334f0c619dcd5ba99c1f5c15772f3522aff
ryanclancy000 <ryanclancy000@gmail.com>
2012-06-19 16:20:32 -0300

    /flight now toggles flight - Added version command - Added PDF file - Change loading of metrics

--- src/main/java/me/ryanclancy000/flight/Flight.java
+++ src/main/java/me/ryanclancy000/flight/Flight.java
@@ -7,126 +7,106 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Flight extends JavaPlugin {
 
     public List flyers = new ArrayList();
+    public PluginDescriptionFile PDF;
     public FlightCommands cHandler = new FlightCommands(this);
 
     @Override
-    public void onDisable() {
-        this.saveConfig();
-    }
-
-    @Override
     public void onEnable() {
-
+        loadMetrics();
+        PDF = this.getDescription();
         getCommand("flight").setExecutor(this);
-
-        try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-            this.getLogger().severe("Could not enable Metrics tracking!");
-        }
-
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("flight")) {
-            return doCommand(sender, args);
-        }
-        return onCommand(sender, cmd, commandLabel, args);
-    }
-
-    private boolean doCommand(CommandSender sender, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (sender instanceof Player) {
             sender = (Player) sender;
         }
 
         if (args.length == 0) {
-            sender.sendMessage(cHandler.pre + ChatColor.GREEN + "by ryanclancy000");
-            sender.sendMessage(cHandler.yellow + "- To view commands, do /flight " + cHandler.green + "help");
+            if (!sender.hasPermission("flight.toggle")) {
+                noPerms(sender);
+                return true;
+            }
+            this.cHandler.toggleCommand(sender, args);
+            return true;
+        }
+        
+        // Version Command
+        if ("version".equalsIgnoreCase(args[0])) {
+            if (!sender.hasPermission("flight.version")) {
+                noPerms(sender);
+                return true;
+            }
+            this.cHandler.version(sender, args);
             return true;
         }
 
         // Help Command
         if ("help".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.help")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.helpCommand(sender, args);
             return true;
-
         }
 
         // Toggle Command
         if ("toggle".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.toggle")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.toggleCommand(sender, args);
             return true;
         }
 
         // On Command
         if ("on".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.on")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.flyOn(sender, args);
             return true;
-
         }
 
         // Off Command
         if ("off".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.off")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.flyOff(sender, args);
             return true;
-
         }
 
         // Check Command
         if ("check".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.check")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.checkCommand(sender, args);
             return true;
         }
 
         // List Command
         if ("list".equalsIgnoreCase(args[0])) {
-
             if (!sender.hasPermission("flight.list")) {
                 noPerms(sender);
                 return true;
             }
-
             this.cHandler.listCommand(sender, args);
             return true;
-
         }
 
         return false;
@@ -134,5 +114,14 @@ public class Flight extends JavaPlugin {
 
     public void noPerms(CommandSender sender) {
         sender.sendMessage(ChatColor.RED + "You do not have permission for that command...");
+    }
+    
+    public void loadMetrics() {
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            this.getLogger().severe("Could not enable Metrics tracking!");
+        }
     }
 }
