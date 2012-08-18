d9ec3c5010ad333ac6b26a78687991e9e0b9bab7
ryanclancy000 <ryanclancy000@gmail.com>
2012-08-18 18:16:13 -0300

    Keep flight enable when a player dies

--- src/main/java/net/umc/flight/FlightListener.java
+++ src/main/java/net/umc/flight/FlightListener.java
@@ -5,15 +5,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class FlightListener implements Listener {
-    
+
     private final Flight plugin;
-    
+
     public FlightListener(Flight plugin) {
         this.plugin = plugin;
     }
-    
+
     @EventHandler
     public void onWorldChange(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
@@ -21,7 +22,15 @@ public class FlightListener implements Listener {
             player.setAllowFlight(true);
         }
     }
-    
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.getFlyers().contains(player.getName())) {
+            player.setAllowFlight(true);
+        }
+    }
+
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
